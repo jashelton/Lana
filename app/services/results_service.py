16d0c0f93576af6722c03d24035a055e74220e25
jashelton <justin.shelton@yahoo.com>
2018-01-24 22:25:39 -0500

    querying for and returning poll results data

--- app/services/results_service.py
+++ app/services/results_service.py
@@ -8,25 +8,42 @@ class ResultsService(BaseService):
     self._db_session = self.new_session()
 
   def get_results_by_poll_id(self, poll_id):
+    # +-------+
+    # | count |
+    # +-------+
     sql_response_count = text(' \
       select count(*) as count \
       from poll_events \
       where poll_id = :poll_id and action = "completed"; \
     ')
 
-    num_responses = self._db_session.execute(sql_response_count, dict(poll_id=poll_id)).fetchone()
-    print(num_responses)
-    return dict(responses=num_responses['count'])
+    # +----------------------+
+    # | id | type | question |
+    # +----------------------+
+    sql_questions = text(' \
+      select id, type, question from questions where poll_id = :poll_id; \
+    ')
 
-    # What I want to return
-    # results: {
-    #   responses: number,
-    #   questions: [{
-    #     id: number,
-    #     question: string,
-    #     response_count: number,
-    #     type?: 'primary' | 'secondary'
-    #   }
-    #   ...
-    #   ]
-    # }
+    # +--------------------------------------------+
+    # | id | question_id | answer | response_count |
+    # +--------------------------------------------+
+    sql_answers_responses = text(' \
+      select A.id, A.question_id, A.answer, count(R.id) as response_count from answers A \
+      left join responses R on R.poll_id = A.poll_id and R.value = A.value \
+      where A.poll_id = :poll_id \
+      group by A.id, A.answer, A.question_id; \
+    ')
+
+    questions = self._db_session.execute(sql_questions, dict(poll_id=poll_id)).fetchall()
+    questions_dict = [dict(zip(row.keys(), row)) for row in questions]
+
+    answers_responses = self._db_session.execute(sql_answers_responses, dict(poll_id=poll_id))
+    answers_responses_dict = [dict(zip(row.keys(), row)) for row in answers_responses]
+
+    num_responses = self._db_session.execute(sql_response_count, dict(poll_id=poll_id)).fetchone()
+
+    for q in questions_dict:
+      q['answers'] = [a for a in answers_responses_dict if a['question_id'] == q['id']]
+
+    
+    return dict(responses=num_responses['count'], questions=questions_dict)
